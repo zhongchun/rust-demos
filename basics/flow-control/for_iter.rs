9fa24ea15da69bc81a9cbe506d79af7bf94c8640
bermaker <zhongchunyu@gmail.com>
2023-03-23 15:09:03 +0800

    add flow-control

---
+++ basics/flow-control/for_iter.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    println!("names: {:?}", names);
+}
