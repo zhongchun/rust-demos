8ec89063f21ca87cf731e5c493371c04dfd7c107
bermaker <zhongchunyu@gmail.com>
2023-04-02 21:54:20 +0800

    optimize minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,10 +4,10 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
